2c4d1a640b65e565334076d73808bd8354b2e032
FreddyPenado <penadofreddy@gmail.com>
2020-05-05 14:40:33 -0600

    title change

--- index.view.php
+++ index.view.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="stilos.css">
-    <title>Formulario de contactod</title>
+    <title>Formulario de contacto</title>
 </head>
 <body>
     <div class="wrap">
