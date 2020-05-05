b7cc8fcbdca66db107952988d1df5addb9a8e3aa
FreddyPenado <penadofreddy@gmail.com>
2020-05-05 14:34:56 -0600

    form desing

---
+++ index.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="stilos.css">
+    <title>Formulario de contactod</title>
+</head>
+<body>
+    <div class="wrap">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+            
+            <input type="text" class="form-control" name="name" id="name" placeholder="Name:" value="">
+            
+            <input type="text" class="form-control" name="email" id="email" placeholder="Email:" value="">
+            
+            <textarea name="message" id="message" class="form-control" placeholder="Message:"></textarea>
+            
+            <input type="submit" name="submit" class="btn-btn-primary" value="Send Email">
+        
+        </form>
+    
+    </div>
+</body>
+</html>
