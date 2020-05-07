774e0b8876ee7241e9328ab65960d862b9493149
FreddyPenado <penadofreddy@gmail.com>
2020-05-07 08:16:37 -0600

    form validate and send mmessage

--- index.php
+++ index.php
@@ -1,5 +1,55 @@
 <?php 
-require 'index.view.php'
+
+$errores = '';
+$enviado = '';
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email =  $_POST['email'];
+    $message = $_POST['message'];
+
+    //name validate
+    if (!empty($name)) {
+        $name = trim($name);
+        $name = filter_var($name ,FILTER_SANITIZE_STRING);
+    }
+    else{
+        $errores .= 'Please, enter a name! <br />';
+    }
+
+    //email validate
+    if (!empty($email)){
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $errores .= 'Please enter a valid email! <br />';
+            }
+    } else  {
+                $errores .= 'Please enter a email! <br />';
+            }
+
+     //message validate       
+     if (!empty($message)) {
+         $message = htmlspecialchars($message);
+         $message = trim($message);
+         $message = stripslashes($message);
+     } else{
+         $errores .= 'Please enter a message! </br>';
+     }   
+
+     if (!$errores) {
+            $send_to = 'yourname@yourcompany.com';
+            $asunto = 'Message send form Mypage.com';
+            $prepare_message = "From: $name \n";
+            $prepare_message .= "TO:  $email \n";
+            $prepare_message .= "Message:" . $message; 
+            
+            //mail($send_to, $asunto, $prepare_message);
+            $enviado = true;
+     }
+}
+
+require 'index.view.php';
 
 
-?>
+?> 
--- index.view.php
+++ index.view.php
@@ -9,14 +9,24 @@
 </head>
 <body>
     <div class="wrap">
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
             
-            <input type="text" class="form-control" name="name" id="name" placeholder="Name:" value="">
+            <input type="text" class="form-control" name="name" id="name" placeholder="Name:" value="<?php if(!$enviado && isset($name)) echo $name ?>">
             
-            <input type="text" class="form-control" name="email" id="email" placeholder="Email:" value="">
+            <input type="text" class="form-control" name="email" id="email" placeholder="Email:" value="<?php if(!$enviado && isset($email)) echo $email ?>" >
             
-            <textarea name="message" id="message" class="form-control" placeholder="Message:"></textarea>
-            
+            <textarea name="message" id="message" class="form-control" placeholder="Message:"><?php if(!$enviado && isset($message)) echo $message ?></textarea>
+
+            <?php if (!empty($errores)): ?>
+                <div class="alert error">
+                    <?php echo $errores; ?>
+                </div> 
+            <?php elseif ($enviado):?> 
+                <div class="alert success">
+                    <p>Message send succesfully!</p>
+                </div> 
+            <?php endif ?>
+
             <input type="submit" name="submit" class="btn-btn-primary" value="Send Email">
         
         </form>
